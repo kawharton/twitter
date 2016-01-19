deceba2dd3835f3252e453cc9d8e7b2cc50470ed
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2016-01-19 15:04:41 -0500

    testing collab

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
-app.get("/", function(req, res, next) {
-	res.send("hey");
+// app.get("/", function(req, res, next) {
+// 	res.send("hey");
+// })
+
+app.get("/dogs", function(req, res, next) {
+	res.send("This is news.");
 })
 
+app.get("/news", function(req, res, next) {
+	res.send("This is news.");
+})
 
+app.use(function (req, res, next) {
+    // do your logging here
+    console.log(req.method, req.url, res.status());
+    
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    
+    next();
+})
 
 
 
